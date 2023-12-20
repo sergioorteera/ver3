47e3c4ee2b8a239e946f205ac8e3a7108cf9061d
Sergio Erazo <sergioorteera@unicauca.edu.co>
2023-12-19 22:29:46 -0500

    avance formularios

--- src/app/aseguramiento/afiliaciones/pages/form-cotizante/form-cotizante.component.ts
+++ src/app/aseguramiento/afiliaciones/pages/form-cotizante/form-cotizante.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { NzUploadChangeParam } from 'ng-zorro-antd/upload';
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 @Component({
   selector: 'app-form-cotizante',
   templateUrl: './form-cotizante.component.html',
   styleUrls: ['./form-cotizante.component.css']
 })
+
 export class FormCotizanteComponent {
+
+  constructor(private msg: NzMessageService) {}
+
+  handleChange(info: NzUploadChangeParam): void {
+
+    if (info.file.status !== 'uploading') {
+      console.log(info.file, info.fileList);
+    }
+
+    if (info.file.status === 'done') {
+      this.msg.success(`${info.file.name} file uploaded successfully`);
+    } else if (info.file.status === 'error') {
+      this.msg.error(`${info.file.name} file upload failed.`);
+    }
+
+  }
 
 }
--- src/app/ngzorro/ngzorro.module.ts
+++ src/app/ngzorro/ngzorro.module.ts
@@ -5,14 +5,15 @@ import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzTabsModule } from 'ng-zorro-antd/tabs';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 
-
-
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzSelectModule } from 'ng-zorro-antd/select';
 import { NzGridModule } from 'ng-zorro-antd/grid';
 import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
-
+import { NzUploadModule } from 'ng-zorro-antd/upload';
+import { NzAlertModule } from 'ng-zorro-antd/alert';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
 
 @NgModule({
   exports: [
@@ -25,6 +26,10 @@ import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
     NzSelectModule,
     NzGridModule,
     NzDatePickerModule,
+    NzUploadModule,
+    NzAlertModule,
+    NzTableModule,
+    NzDividerModule,
 
   ]
 })
